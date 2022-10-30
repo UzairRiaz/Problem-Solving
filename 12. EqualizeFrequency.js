d303cf4de0cd7c7d371b68616bc981d042313031
Uzair Riaz <uzairriaz.pk@gmail.com>
2022-10-30 21:12:37 +0500

    12

--- 12. EqualizeFrequency.js
+++ 12. EqualizeFrequency.js
@@ -1,33 +1,31 @@
-// You are allowed to remove maximum 1 character to equalize the frequency of the String. (if Possible =>True)
+// You must remove 1 character to equalize the frequency of the String. (if Possible =>True)
 
 
-const string = "ssswwwiiijjjaaa";
+const string = "bbcccaa";
 
 const EqualizeFrequency = (str) => {
-    const dir = {};
-    let diff = 0;
-    let arr = [];
-    let change = 0;
-    str.split("").map((s) => {
-        if (dir[s]) {
-            dir[s] = dir[s] + 1;
-        } else {
-            dir[s] = 1;
+    for (let i = 0; i < str.length; i++) {
+        const hash = {}
+        for (let j = 0; j < str.length; j++) {
+            if (i !== j) {
+                const ch = str[j]
+                hash[ch] = (hash[ch] || 0) + 1
+            }
         }
-    })
-    for (key in dir) {
-        arr.push(dir[key]);
-    }
-    arr.map((a) => {
-        diff = Math.abs(arr[0] - a)
-        if (diff) {
-            change++;
+
+        const hashSet = new Set()
+
+        for (let prop in hash) {
+            hashSet.add(hash[prop])
         }
-    })
-    if (change > 1 || diff > 1) {
-        return false;
+        if (hashSet.size === 1) {
+            return true
+        }
     }
-    return true
-}
+
+
+    return false
+};
+
 
 console.log(EqualizeFrequency(string))
