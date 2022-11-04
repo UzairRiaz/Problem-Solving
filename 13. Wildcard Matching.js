a7df665179fdd461de999397b0b011b6cd46af13
Uzair Riaz <uzairriaz.pk@gmail.com>
2022-11-05 00:23:41 +0500

    13

--- 13. Wildcard Matching.js
+++ 13. Wildcard Matching.js
@@ -2,7 +2,23 @@ const s = "aa";
 const p = "*";
 
 const wildCardMatcher = (s, p) => {
-    // TO DO
+    let sIndex = 0;
+    let pIndex = 0;
+
+    while (sIndex <= s.length) {
+        if (s[sIndex] !== p[pIndex]) {
+            if (p[pIndex] !== '*' && p[pIndex] !== '?') {
+                return false;
+            }
+        }
+        if (p[pIndex] === '*') {
+            sIndex++;
+        } else {
+            sIndex++;
+            pIndex++;
+        }
+    }
+    return true
 }
 
 console.log(wildCardMatcher(s, p));
