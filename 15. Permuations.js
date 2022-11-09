609e271785dde5d17ec6079f49e4257e14f22d13
Uzair Riaz <uzairriaz.pk@gmail.com>
2022-11-09 22:49:33 +0500

    15

---
+++ 15. Permuations.js
@@ -0,0 +1,23 @@
+const arr = [1];
+
+const permuataions = (arr) => {
+    const result = [];
+
+    const permute = (arr, m = []) => {
+        if (arr.length === 0) {
+            result.push(m);
+        } else {
+            for (let i = 0; i < arr.length; i++) {
+                let curr = arr.slice();
+                let next = curr.splice(i, 1);
+                permute(curr.slice(), m.concat(next));
+            }
+        }
+    };
+
+    permute(arr);
+
+    return result;
+}
+
+console.log(permuataions(arr));
