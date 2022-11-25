ffe30e7aa4271a9378ddf65d6e763e23c64ef394
Uzair Riaz <uzairriaz.pk@gmail.com>
2022-11-25 22:55:02 +0500

    17

--- 16. MaximumWater.js
+++ 16. MaximumWater.js
@@ -1,5 +1,6 @@
 const arr = [1, 3, 5, 7, 9, 0, 7, 6, 8, 9, 1];
 
+// big O(n^2) - My solution
 const maxWater = (arr) => {
     let max = 0;
     for (let i = 0; i <= arr.length; i++) {
@@ -8,6 +9,25 @@ const maxWater = (arr) => {
             let y = arr[i] > arr[j] ? arr[j] : arr[i];
             let area = x * y;
             max = max > area ? max : area;
+        }
+    }
+    return max;
+}
+
+// big O: O(n) - Not my solution
+const maxWater2 = (arr) => {
+    let max = 0;
+    let i = 0;
+    let j = arr.length - 1;
+    while (i < j) {
+        let x = j - i;
+        let y = arr[i] > arr[j] ? arr[j] : arr[i];
+        let area = x * y;
+        max = max > area ? max : area;
+        if (arr[i] > arr[j]) {
+            j--;
+        } else {
+            i++;
         }
     }
     return max;
