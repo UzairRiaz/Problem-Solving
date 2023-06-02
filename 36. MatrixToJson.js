bcfbe0bba13bd7203587053379370021b0150cc8
uzairriaz <uzairriaz.pk@gmail.com>
2023-06-02 16:10:59 +0500

    36

--- 36. MatrixToJson.js
+++ 36. MatrixToJson.js
@@ -14,21 +14,11 @@ const matrixToJson = (matrix) => {
     let values = matrix.slice(1);
     for (let i = 0; i < values.length; i++) {
         let obj = {};
-<<<<<<< HEAD
         for (let j = 0; j < keys.length; j++) {
             obj[keys[j]] = values[i][j];
         }
         result.push(obj);
     }
     return result;
-=======
-        let rec = matrix[i];
-        for (let j = 0; j < rec.length; j++) {
-            obj[matrix[0][j]] = rec[j]
-        }
-        json.push(obj);
-    }
-    return json;
->>>>>>> 414e94cd8c16b94c2f8231fe2d7fd3aeab36dd98
 }
 console.log(matrixToJson(matrix))
