f89bd9bfa7b20e195f2360b5b12d86171c720238
Uzair <uzairriaz@github.com>
2023-06-04 03:17:14 +0930

    39

---
+++ 39. HOC.js
@@ -0,0 +1,33 @@
+
+const fun1 = (a, b) => {
+    a = a + '-1';
+    b = b + '-1';
+    return [a, b];
+};
+
+const fun2 = (a, b) => {
+    a = a + '-2';
+    b = b + '-2';
+    return [a, b];
+}
+
+const fun3 = (a, b) => {
+    a = a + '-3';
+    b = b + '-3';
+    return [a, b];
+}
+
+const fun4 = (a, b) => {
+    a = a + '-4';
+    b = b + '-4';
+    return [a, b];
+}
+
+const HOC = (...funs) => (a, b) => {
+    funs.forEach(fun => {
+        [a, b] = fun(a, b);
+    });
+    return [a, b];
+}
+
+const [finalA, finalB] = HOC(fun1, fun2, fun3, fun4)('a', 'b');
