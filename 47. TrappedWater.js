e72627233263cdffc40ec0ce8038dcd5bf5b6f57
uzairriaz <uzairriaz.pk@gmail.com>
2023-06-26 12:06:19 +0500

    47

--- 47. TrappedWater.js
+++ 47. TrappedWater.js
@@ -1,10 +1,9 @@
 let h = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1];
 
 const trappedWater = (height) => {
-    // Create Segments
     let segments = [];
     let currentSegement = [];
-    for (let i = 0; i <= height.length; i++) {
+    for (let i = 0; i < height.length; i++) {
         if (currentSegement.length) {
             if (height[i] < currentSegement[0]) {
                 currentSegement.push(height[i]);
@@ -18,6 +17,16 @@ const trappedWater = (height) => {
             currentSegement.push(height[i])
         }
     }
+    let totalWater = 0;
+    for (let i = 0; i < segments.length; i++) {
+        let segment = segments[i];
+        let min = Math.min(segment[0], segment[segment.length - 1]);
+        for (let j = 1; j < segment.length - 1; j++) {
+            if (segment[j] === undefined) continue;
+            totalWater += min - segment[j];
+        }
+    }
+    return totalWater;
 };
 
-trappedWater(h);
+console.log(trappedWater(h))
